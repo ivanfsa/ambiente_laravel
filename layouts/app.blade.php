77d70b3096d5435380eeac3900d509b2ba647c5c
ivanfsa <ivanfsa@gmail.com>
2019-11-19 23:58:26 -0300

    Update app.blade.php

--- layouts/app.blade.php
+++ layouts/app.blade.php
@@ -1,27 +1,30 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE html>
+{{-- <html lang="{{ config('app.locale') }}"> --}}
+<html lang="pt-br">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    @include('layouts.head')
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  
-    <div class="conteinar">
-        @yield('navegacao')
-        @yield('conteudo')
-        @yield('rodape')
-    </div>
+    <body>
+        <style>
+            body {
+                padding-top: 60px;
+                padding-left: 50px;
+                padding-right: 50px;
+            }
+        </style>
+        
+        @include('layouts.menu')
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+        <div class="container">
+
+            @yield("conteudo")
+
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <!-- <script src="{{ URL::asset('js/jquery-3.1.1.min.js') }}" charset="utf-8" ></script> -->
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}" charset="utf-8" ></script>
+        @yield("javascript")
+
+    </body>
 </html>
