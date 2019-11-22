3c53388d4f2a5739a7feeacb26ff4c2da5cf93db
ivanfsa <ivanfsa@gmail.com>
2019-11-21 21:00:06 -0300

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,1 +1,9 @@
-//em construção
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo com Laravel</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/estilo.css') }}">
+    
+</head>
