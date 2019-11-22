9bf94604c876467420c7f9d39481edd16c422b36
ivanfsa <ivanfsa@gmail.com>
2019-11-22 15:06:07 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 {{-- <html lang="{{ config('app.locale') }}"> --}}
 <html lang="pt-br">
 
+  <!-- Importando o arquivo head.blade.php -->
     @include('layouts.head')
 
     <body>
@@ -13,17 +14,32 @@
             }
         </style>
         
+        <!-- Importando o arquivo menu.blade.php -->
         @include('layouts.menu')
 
         <div class="container">
 
+            <!-- Espaço reservado para o conteúdo dos outros arquivos blade.php 
+                 para usar esse espaço o arquivo precisa abrir uma sessão: @section('conteudo') 
+                 OBS: dentro do arquivo home.blade.php tem o exemplo de como usar 
+            -->
             @yield("conteudo")
 
         </div>
 
+        <!-- Usando o JQyuery OnLine -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <!-- <script src="{{ URL::asset('js/jquery-3.1.1.min.js') }}" charset="utf-8" ></script> -->
+
+        <!-- você pode copiar o arquivo JQuery para o seu projeto e informará esse comando 
+            <script src="{{ URL::asset('js/jquery-3.1.1.min.js') }}" charset="utf-8" ></script> 
+        -->
         <script src="{{ URL::asset('js/bootstrap.min.js') }}" charset="utf-8" ></script>
+
+        <!-- Similar o "conteudo", esse espaço será reservado para arquivos JavaScript 
+                 nos outros arquivos blade.php 
+                 para usar esse espaço o arquivo precisa abrir uma sessão: @section('javascript') 
+                 OBS: dentro do arquivo home.blade.php tem o exemplo de como usar 
+            -->
         @yield("javascript")
 
     </body>
