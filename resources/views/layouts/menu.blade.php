2052bc628bce183df7b56dd9aca9325b4139f9d3
ivanfsa <ivanfsa@gmail.com>
2019-11-21 21:00:31 -0300

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,1 +1,18 @@
-//em construção
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Relatório <span class="sr-only">(current)</span></a>
+                </li>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
