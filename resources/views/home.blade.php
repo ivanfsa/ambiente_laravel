e9437efa562dc974670518bde44e0d0231d60505
ivanfsa <ivanfsa@gmail.com>
2020-07-23 20:42:59 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
           <h1>Lista de tarefas</h1>
         </div>
-        <form role="form" action="{{ route('tarefa.store') }}" method="POST">
+        <form role="form" action="#" method="POST">
           {{ csrf_field() }}
           <div class="row">
             <div class="col">
@@ -19,27 +19,14 @@
         </form>
       </div>
       {{-- exibir mensagem de successo --}}
-        @if (Session::has('successo'))
-          <div class="alert alert-success">
-            <strong>Successo:</strong> {{ Session::get('successo') }}
-          </div>
-        @endif
+       
 
         {{-- exibir mensagem de erro --}}
-        @if (count($errors) > 0)
-          <div class="alert alert-danger">
-            <strong>Error:</strong>
-            <ul>
-              @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
+        
       <div class="container">
           <div class="row">
           {{-- exibir tarefas quando criadas --}}
-          @if (count($tarefas) > 0)
+          
             <table class="table table-hover ">
               <thead>
                 <tr>
@@ -49,27 +36,9 @@
                   <th>Excluir</th>
                 </tr>
               </thead>
-              @foreach ($tarefas as $tarefa)
-              <tbody>
-                <tr>
-                  <th scope="row">{{ $tarefa->id }}</th>
-                  <td>{{ $tarefa->descricao }}</td>
-                  <td><a href="{{ route('tarefa.edit', ['tarefa'=>$tarefa->id]) }}" class='btn btn-info'>Editar</a></td>
-                  <!-- href="{ { route('tasks.edit', ['tasks'=>$storedTask->id]) "  -->
-                  <td>
-                   <!-- action="{ { route('tasks.destroy', ['tasks'=>$storedTask->id]) " -->
-                    <form action="{{ route('tarefa.destroy', ['tarefa'=>$tarefa->id]) }}" method='POST'>
-                      {{ csrf_field() }}
-                      <input type="hidden" name='_method' value='DELETE'>
-
-                      <input type="submit" class='btn btn-danger' value='Excluir'>
-                    </form>
-                  </td>
-                </tr>
-              </tbody>
-              @endforeach
+              
             </table>
-            @endif
+            
           </div>
       </div>
 
